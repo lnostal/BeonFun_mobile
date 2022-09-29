0e982b03d922278266f89e94901da10cde98c927
nosta <lnostal@yandex.ru>
2022-09-29 10:57:30 +0300

    create unread discussions page

---
+++ lib/modules/main_tabbar/tab_profile/unread_discussions_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../models/post.dart';
+import '../../expanded_post/post_expanded_page.dart';
+
+class UnreadDiscussionsPage extends StatefulWidget {
+  List<Post> posts;
+
+  UnreadDiscussionsPage({super.key, required this.posts});
+
+  @override
+  State<UnreadDiscussionsPage> createState() => _UnreadDiscussionsPageState();
+}
+
+class _UnreadDiscussionsPageState extends State<UnreadDiscussionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(middle: Text('Discussions')),
+      child: ListView.builder(
+          itemCount: widget.posts.length,
+          itemBuilder: (context, index) {
+            return getDiscussionCell(index);
+          }),
+    );
+  }
+
+  Widget getDiscussionCell(index) {
+    return TextButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => openExpandedPostPage(widget.posts[index])));
+        },
+        child: Text(
+          widget.posts[index].title,
+          textAlign: TextAlign.left,
+          style: const TextStyle(fontSize: 16),
+        ));
+  }
+
+  PostExpandedPage openExpandedPostPage(Post post) {
+    String blname = post.type == PostType.forum ? '' : post.blogInfo!.stringId;
+    String id = post.type == PostType.forum
+        ? post.globalId.toString()
+        : post.inPostId.toString();
+
+    return PostExpandedPage(blname: blname, id: id, type: post.type);
+  }
+}
